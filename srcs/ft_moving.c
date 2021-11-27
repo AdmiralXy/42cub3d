97071c0e480eb386b9961dbc61b9ab059d0ff2f4
AdmiralXy <gameproblem80@gmail.com>
2021-11-27 23:42:18 +0300

    add calculation stop if position unchanged

--- srcs/ft_moving.c
+++ srcs/ft_moving.c
@@ -2,17 +2,23 @@
 
 int	ft_move_forward(t_env *env)
 {
-	int	x_step;
-	int	y_step;
+	int		x_step;
+	int		y_step;
+	double	old_pos_x;
+	double	old_pos_y;
 
 	x_step = (int)(env->p.pos_x + env->p.dir_x * env->p.move_speed);
 	y_step = (int)(env->p.pos_y + env->p.dir_y * env->p.move_speed);
+	old_pos_x = env->p.pos_x;
+	old_pos_y = env->p.pos_y;
 	if (x_step <= 24 && y_step <= 24 && x_step > 0 && y_step > 0)
 	{
 		if (env->world_map[x_step][(int)(env->p.pos_y)] == 0)
 			env->p.pos_x += env->p.dir_x * env->p.move_speed;
 		if (env->world_map[(int)(env->p.pos_x)][y_step] == 0)
 			env->p.pos_y += env->p.dir_y * env->p.move_speed;
+		if (old_pos_x == env->p.pos_x && old_pos_y == env->p.pos_y)
+			return (0);
 		return (1);
 	}
 	return (0);
@@ -20,17 +26,23 @@ int	ft_move_forward(t_env *env)
 
 int	ft_move_back(t_env *env)
 {
-	int	x_step;
-	int	y_step;
+	int		x_step;
+	int		y_step;
+	double	old_pos_x;
+	double	old_pos_y;
 
 	x_step = (int)(env->p.pos_x - env->p.dir_x * env->p.move_speed);
 	y_step = (int)(env->p.pos_y - env->p.dir_y * env->p.move_speed);
+	old_pos_x = env->p.pos_x;
+	old_pos_y = env->p.pos_y;
 	if (x_step <= 24 && y_step <= 24 && x_step > 0 && y_step > 0)
 	{
 		if (env->world_map[x_step][(int)(env->p.pos_y)] == 0)
 			env->p.pos_x -= env->p.dir_x * env->p.move_speed;
 		if (env->world_map[(int)(env->p.pos_x)][y_step] == 0)
 			env->p.pos_y -= env->p.dir_y * env->p.move_speed;
+		if (old_pos_x == env->p.pos_x && old_pos_y == env->p.pos_y)
+			return (0);
 		return (1);
 	}
 	return (0);
