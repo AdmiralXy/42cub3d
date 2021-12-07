b8e087f13b1899be02db8fef04e349fc3c4747cf
AdmiralXy <gameproblem80@gmail.com>
2021-12-07 15:40:10 +0300

    adjust main

--- srcs/ft_main.c
+++ srcs/ft_main.c
@@ -13,11 +13,9 @@ void write_map(t_env *env)
 			printf("%d", env->world_map[cnt][k]);
 			k++;
 		}
-		free(env->world_map[cnt]);
 		puts("");
 		cnt++;
 	}
-	free(env->world_map);
 }
 int	main(int ac, char **av)
 {
@@ -31,19 +29,19 @@ int	main(int ac, char **av)
 	(void)av;
 	env = malloc(sizeof(t_env));
 	//env->map_name = ft_strdup(argv[1]);
-	env->map_name = ft_strdup("map.cub");
-	//  if (!ft_initialize(env))
-	//  	return (1);
+	env->map_name = ft_strdup("new.cub");
+	if (!ft_initialize(env))
+		return (1);
 	get_path(env);
 	rewrite_map(env);
 	write_map(env);
+	//printf("map: %d\n", env->world_map[8][3]);
 	//  if (!ft_parser(env))
 	//  	return (1);
-	//  ft_initialize_graphics(env);
-	//  ft_minimap(env);
-	//  ft_raycasting(env);
-	//  mlx_loop(env->mlx);
-	// return (0);
+	ft_initialize_graphics(env);
+	ft_minimap(env);
+	ft_raycasting(env);
+	mlx_loop(env->mlx);
 	free(env->map_name);
 	free(env);
 }
